169150d253ea00d28e6e483c73178523ba1e2460
user.name <roberthbardales@hotmail.com>
2024-03-01 22:19:21 -0500

    posts funcionando

--- blogfunciones/applications/blog/urls.py
+++ blogfunciones/applications/blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 
 from .import views
-from .views import home,generales,programacion,tutoriales
+from .views import home,generales,programacion,tutoriales,detallePost
 
 app_name = "blog_app"
 urlpatterns = [
@@ -12,5 +12,6 @@ urlpatterns = [
     path('generales/',generales,name="generales"),
     path('programacion/',programacion,name="programacion"),
     path('tutoriales/',tutoriales,name="tutoriales"),
+    path('<slug:slug>',detallePost,name="detalle_post"),
 
 ]
--- blogfunciones/applications/blog/views.py
+++ blogfunciones/applications/blog/views.py
@@ -1,18 +1,67 @@
 from django.shortcuts import render
+from django.http import Http404
+from django.db.models import Q
+from django.core.paginator import Paginator
+
+
 
 # Create your views here.
 
+from .models import Post,Autor,Categoria
 
 def home(request):
-    return render(request,'index.html')
+    queryset=request.GET.get("buscar")
+    print(f"la consulta es: {queryset}")
+    post=Post.objects.filter(estado=True)
+    if queryset:
+        post=Post.objects.filter(
+            Q(titulo__icontains=queryset) |
+            Q(descripcion__icontains=queryset)
+        ).distinct()
+
+    contexto={'contexto_post':post}
+
+    paginator=Paginator(post,2)
+    page=request.GET.get('page')
+    post=Paginator
+
+    return render(request,'index.html',contexto)
+
+
+def detallePost(request,slug):
+    try:
+        post=Post.objects.get(
+            slug=slug
+            )
+        contexto={'detalle_post':post}
+        return render(request,'post.html',contexto)
+    except Post.DoesNotExist:
+        raise Http404
+
 
 def generales(request):
-    return render(request,'generales.html')
+    post=Post.objects.filter(
+        estado=True,
+        categoria=Categoria.objects.get(nombre__iexact='General')
+        )
+    contexto={'contexto_post':post}
+    return render(request,'generales.html',contexto)
 
 def programacion(request):
-    return render(request,'programacion.html')
+    post=Post.objects.filter(
+    estado=True,
+    categoria=Categoria.objects.get(nombre__iexact='Programacion')
+    )
+    contexto={'contexto_post':post}
+    return render(request,'programacion.html',contexto)
 
 def tutoriales(request):
-    return render(request,'tutoriales.html')
+    post=Post.objects.filter(
+    estado=True,
+    categoria=Categoria.objects.get(nombre__iexact='Tutorial')
+    )
+    contexto={'contexto_post':post}
+    return render(request,'tutoriales.html',contexto)
+
 
 
